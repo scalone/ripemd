b501bc732bdc014a9cebb4f4523ea14a32785b39
Thiago Scalone <thiago@scalone.com.br>
2015-08-03 21:58:00 -0300

    Fix byte and word definition.

--- rmd160.h
+++ rmd160.h
@@ -23,8 +23,8 @@
 /* typedef 8 and 32 bit types, resp.  */
 /* adapt these, if necessary,
    for your operating system and compiler */
-typedef    unsigned char        byte;   /* unsigned 8-bit integer */
-typedef    unsigned short       word;   /* unsigned 16-bit integer */
+typedef    uint8_t        byte;   /* unsigned 8-bit integer */
+typedef    uint16_t       word;   /* unsigned 16-bit integer */
 typedef    uint32_t        dword;  /* unsigned 32-bit integer */
 
 /********************************************************************/
